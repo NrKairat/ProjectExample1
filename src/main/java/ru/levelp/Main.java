134375eae9a9c12e4f0a2446eb0306bdd158a396
specvap <nurum1@mail.ru>
2017-01-18 02:00:50 +0300

    Initial commit

---
+++ src/main/java/ru/levelp/Main.java
@@ -0,0 +1,24 @@
+package ru.levelp;
+
+import org.hibernate.Session;
+
+/**
+ * Created by кайрат on 17.01.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+//            HibernateManager hibernateManager = HibernateManager.getInstance();
+//            Session session = hibernateManager.getSession();
+//
+//            User user = new User();
+//
+//        session.beginTransaction();
+//
+//        session.saveOrUpdate(user);
+//
+//        session.getTransaction().commit();
+
+    }
+
+}
